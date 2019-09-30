a392ebe9db3892613826b4b371669977899a47f6
falksinke <falksinke@gmail.com>
2019-09-30 10:16:10 +0200

    Working linked list implementation, temporary identifier implementation and interface

--- AP2-Skeleton/src/main/java/nl/vu/labs/phoenix/ap/IdentifierInterface.java
+++ AP2-Skeleton/src/main/java/nl/vu/labs/phoenix/ap/IdentifierInterface.java
@@ -1,20 +1,54 @@
 package nl.vu.labs.phoenix.ap;
 
-/* [1] Identifier Specification
- * 	   -- Complete the specification for an Identifier interface.
- * 		  See the List interface for inspiration
- */
 public interface IdentifierInterface {
-	/* 
-	 * [2] Mandatory methods. Make sure you do not modify these!
-	 * 	   -- Complete the specifications of these methods
+    /*
+    Elements: characters of type char saved in a stingbuffer
+    Structure: Linear
+    Domain: Only alphanumeric characters, identifier begins with a letter,
+            identifier has length of at least 1 character
+
+     constructors
+
+        * Identifier(); *
+        PRE  -
+        *   POST -A new Identifier has been made and contains the character x *
+        *
+        Identifier
+        PRE  -  *
+        POST - A new Identifier-object has been made and contains a copy of src.
+
+    */
+
+	void init();
+	/*
+	 * PRE -
+	 * POST - Identifier has one char
 	 */
-	
+
+	boolean add(char c);
+	/*
+	 * PRE -
+	 * POST - true: the char falls inside the domain and is added
+	 * 		  false: the char falls outside of the domain and isn't added
+	 */
+
 	String value();
-	
-	/* 
-	 * [3] Add anything else you think belongs to this interface 
+	/*
+	 * PRE -
+	 * POST - The string that makes up the identifier has been returned
 	 */
-	
-	// your code here
+
+	int size();
+	/*
+	 * PRE -
+	 * POST - the size of the Identifier is returned
+	 */
+
+	boolean equals(Identifier comparand);
+	/*
+	 * PRE - the identifiers need to have the same size
+	 * POST - true: The identifier is the same as the identifier passed
+	 * 		  false: The identifier is not the same as the identifier passed
+	 */
+
 }
