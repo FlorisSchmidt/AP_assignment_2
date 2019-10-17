9e451ff2473d53271afd3b38aa3034bbb7c67b1c
floris.schmidt <floris.schmidt@hotmail.com>
2019-10-17 16:55:12 +0200

    Removed redundant testDomain method

--- AP2-Skeleton/src/main/java/nl/vu/labs/phoenix/ap/Identifier.java
+++ AP2-Skeleton/src/main/java/nl/vu/labs/phoenix/ap/Identifier.java
@@ -1,7 +1,6 @@
 package nl.vu.labs.phoenix.ap;
 
 import java.lang.StringBuffer;
-import java.lang.Character;
 
 public class Identifier implements IdentifierInterface {
 
@@ -28,26 +27,13 @@ public class Identifier implements IdentifierInterface {
 	}
 
 	@Override
-	public boolean add(char c) throws IdentifierException{
-		if(testDomain(c) && addWasCalled) {
+	public boolean add(char c){
+		if(addWasCalled) {
 			sb.append(c);
-			return true;
-		}
-		if(testDomain(c) && !addWasCalled) {
+		} else {
 			sb.delete(0, 1);
 			sb.append(c);
 			addWasCalled = true;
-			return true;
-		}
-		return false;
-	}
-
-	private boolean testDomain(char c) throws IdentifierException {
-		if(!addWasCalled && Character.isDigit(c)) {
-			throw new IdentifierException("first element can't be a digit");
-		}
-		if(!Character.isDigit(c) && !Character.isLowerCase(c) && !Character.isUpperCase(c)){ //TODO this is never called!
-			throw new IdentifierException("unsupported character '" + c + "'");
 		}
 		return true;
 	}
