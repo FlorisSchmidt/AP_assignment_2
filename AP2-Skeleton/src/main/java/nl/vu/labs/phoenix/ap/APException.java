602b5fa0fb8ed14cc9cd68ea2919c404103124b2
floris.schmidt <floris.schmidt@hotmail.com>
2019-10-17 17:20:29 +0200

    Removed comment

--- AP2-Skeleton/src/main/java/nl/vu/labs/phoenix/ap/APException.java
+++ AP2-Skeleton/src/main/java/nl/vu/labs/phoenix/ap/APException.java
@@ -1,13 +1,5 @@
 package nl.vu.labs.phoenix.ap;
 
-//public class APException extends Exception {
-//
-//    private static final long serialVersionUID = 1L;
-//
-//    public APException (String s) {
-//        super(s);
-//    }
-//}
 class APException extends Exception{
     APException(String s){
         super(s);
